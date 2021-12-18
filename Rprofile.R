fb74c8a906ff95c0890d58644bf666fe301e5f71
olajideajayi <94552752+olajideajayi@users.noreply.github.com>
2021-12-17 19:06:28 -0500

    Add files via upload

---
+++ Rprofile.R
@@ -0,0 +1,5 @@
+#APIs, IDs, Keys & Passwords
+Azure_SubID<-"xxxxxxxxxxxxxxxxxx"
+Azure_tenantID<-"xxxxxxxxxxxxxxxxxxxxxx"
+Azure_ResourceGrp<-"xxxxxxxxxxxxxxxxxxx"
+Azure_Storage_Key<-"xxxxxxxxxxxxxxxxxxxxxxxxxxx"
